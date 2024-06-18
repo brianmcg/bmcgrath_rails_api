6b08ca53705129a1df1255560f264b823b34ff35
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-18 13:52:47 +0100

    Fixed controller

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,24 +1,15 @@
 class EmailsController < ApplicationController
 
-  def index
-    response = { message: 'success' }
-    render json: response, status: :created
-  end
-
   # POST /emails
   def create
     api_key = "c282ddb00d4a003d89f4811f087abe03"
     api_host = "send.api.mailtrap.io"
 
-    email_name = params.require(:name)
-    email_address = params.require(:address)
-    email_message = params.require(:message)
-
     mail = Mailtrap::Mail::Base.new(
       from: { email: "mailtrap@bmcgrath.net", name: "bmcgrath" },
       to: [{ email: "brian.joseph.mcgrath@gmail.com" }],
-      subject: "Message from #{email_name} <#{email_address}>",
-      text: email_message
+      subject: "Message from #{email_params[:name]} <#{email_params[:address]}>",
+      text: email_params[:message]
     )
 
     client = Mailtrap::Client.new(api_key: api_key, api_host: api_host)
@@ -26,5 +17,11 @@ class EmailsController < ApplicationController
     response = client.send(mail)
 
     render json: response, status: :created
+  end
+
+  private
+
+  def email_params
+    params.require(:email).permit(:name, :address, :message)
   end
 end
