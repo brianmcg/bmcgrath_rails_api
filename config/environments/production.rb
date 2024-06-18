aebead2aec2c10a7f787e94473e32df42e3e2c7a
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-18 13:50:09 +0100

    Added host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,10 +75,10 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "bmcgrath.centralindia.cloudapp.azure.com",     # Allow requests from example.com
+    # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-end
+  end
