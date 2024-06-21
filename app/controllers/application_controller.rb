43bdc3b4c091a98dc092f3160c4586813fb06935
Brian McGrath <brian.joseph.mcgrath@gmail.com>
2024-06-21 21:30:08 +0100

    Added Basic Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+	include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name: Rails.application.credentials.api_user, password: Rails.application.credentials.api_pass
 end
